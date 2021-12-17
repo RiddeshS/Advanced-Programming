fcb33dfac859dd779ffd2380db97409a03b19e8f
RiddeshS <sonawane.riddesh.15et1043@gmail.com>
2021-12-17 18:07:21 +0000

    Importing React and Axios and definig path and service url for API calling with defining a redux with state and action"

---
+++ client/src/MainPage.js
@@ -0,0 +1,12 @@
+import React, { useState, useReducer, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
+const API_ENDPOINT = "/";
+const TRENDS = "api/trends?id="
+const LOCATION_ID = "1"
+
+const trendsReducer = (state,action)=>{
+	switch(action.type){
+
+	}
+};
